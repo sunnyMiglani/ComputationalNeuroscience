bef860801632e497586fd352ec4a200571d7bab3
Sunny Miglani <sunny.miglani.2015@my.bristol.ac.uk>
2018-02-27 19:11:00 +0000

    arrays are bad

--- course/auto/seven_segments.py
+++ course/auto/seven_segments.py
@@ -1,5 +1,6 @@
 from math import *
 
+
 def seven_segment(pattern):
 
     def to_bool(a):
@@ -38,14 +39,14 @@ def seven_segment(pattern):
 
     pattern_b=map(to_bool,pattern) # applies the to_bool function over the pattern array
 
-    hor(pattern_b[0])
-    vert(pattern_b[1],pattern_b[2],pattern_b[3])
-    vert(pattern_b[4],pattern_b[5],pattern_b[6])
+    hor(pattern_b[0]) # horizontal rows
+    vert(pattern_b[1],pattern_b[2],pattern_b[3]) #vertical row 1
+    vert(pattern_b[4],pattern_b[5],pattern_b[6]) #vertical row 2
 
     number=0
     for i in range(0,4):
-        if pattern_b[7+i]:
-            number+=pow(2,i)
+        if pattern_b[7+i]: # look at the number (0 = tru?)
+            number+=pow(2,i) # binary
     print(int(number))
 
 
@@ -54,17 +55,26 @@ six=[1,1,-1,1,1,1,1,-1,1,1,-1]
 three=[1,-1,1,1,-1,1,1,1,1,-1,-1]
 one=[-1,-1,1,-1,-1,1,-1,1,-1,-1,-1]
 
+print("-------------")
 seven_segment(three)
+print("-------------")
 seven_segment(six)
+print("-------------")
 seven_segment(one)
+print("-------------")
+
+print("Hopfield Three")
+HopfieldNetwork(three)
 
 
+print("-------------")
 print("test1")
 
 test=[1,-1,1,1,-1,1,1,-1,-1,-1,-1]
 
 seven_segment(test)
 
+print("-------------")
 print("test2")
 
 #here the network should run printing at each step
--- course/auto/seven_segments_p3.py
+++ course/auto/seven_segments_p3.py
@@ -1,19 +1,38 @@
 from math import *
 
+def HopfieldNetwork(pattern):
+    print ("Hello world! \n")
+
+    weightMatrix = []
+    this_sum = 0;
+    for val_i in range(0,len(pattern)):
+        for val_j in range(0,len(pattern)):
+            if(val_i == val_j): continue;
+            this_sum += (pattern[val_i] * pattern[val_j])
+
+        this_sum = this_sum / len(pattern)
+        weightMatrix[val_i].append(this_sum)
+        this_sum = 0;
+
+    print(this_sum)
+    print(weightMatrix)
+
+
+
 def seven_segment(pattern):
 
     def to_bool(a):
         if a==1:
             return True
         return False
-    
+
 
     def hor(d):
         if d:
             print(" _ ")
         else:
             print("   ")
-    
+
     def vert(d1,d2,d3):
         word=""
 
@@ -21,20 +40,20 @@ def seven_segment(pattern):
             word="|"
         else:
             word=" "
-        
+
         if d3:
             word+="_"
         else:
             word+=" "
-        
+
         if d2:
             word+="|"
         else:
             word+=" "
-        
+
         print(word)
 
-    
+
 
     pattern_b=list(map(to_bool,pattern))
 
@@ -47,7 +66,7 @@ def seven_segment(pattern):
         if pattern_b[7+i]:
             number+=pow(2,i)
     print(int(number))
-        
+
 
 
 six=[1,1,-1,1,1,1,1,-1,1,1,-1]
@@ -57,6 +76,9 @@ one=[-1,-1,1,-1,-1,1,-1,1,-1,-1,-1]
 seven_segment(three)
 seven_segment(six)
 seven_segment(one)
+
+print("Hopfield Matrix! ")
+HopfieldNetwork(three)
 
 
 print("test1")
