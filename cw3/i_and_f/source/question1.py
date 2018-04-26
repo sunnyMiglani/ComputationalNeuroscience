0846a6ec7ea856ce6ae2beaa56c062175d403f45
Sunny Miglani <sunny.miglani.2015@my.bristol.ac.uk>
2018-04-26 23:07:44 +0100

    added units and formula

--- cw3/i_and_f/source/question1.py
+++ cw3/i_and_f/source/question1.py
@@ -1,1 +1,30 @@
-# Potato 
+import numpy as np;
+
+'''
+Aim is to simulate an integrate and fire neuron.
+This model uses a system of equations to calculate whether a neuron spikes based
+on a specific threshold value.
+
+The following parameters have been set and defined:
+
+timeConstant = 10ms;
+E_l = -70mV;
+resetVoltage = -70mV;
+thresholdVoltage = =40mV;
+R_m = 10 MOhm;
+InputCurrent = 3.1 nA;
+timesteps = 1ms;
+
+The main formula used for the Integrate and fire model is this:
+
+t_m V' = E_l - V + Rm*I
+
+'''
+## Defining units in the system
+
+
+ms = 0.001;
+mV = 0.001;
+
+nA = 10**(-9);
+mOhm = 10**6;
